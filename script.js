6cb208bb951bdc1ba5bb799c3bc25212198d9122
nookesh <nookeshkarri7@gmail.com>
2021-09-09 15:22:37 +0530

    Whatsapp sharing link updated

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ nameEl.textContent="Hi.. "+inputName.toUpperCase()
 let color=["red","blue","yellow","Orange","green"]
 
 let whatsapplinkEl=document.getElementById("whatsapplink")
-whatsapplinkEl.href="whatsapp://send?text= *మీరు చేసే ప్రతీ కార్యం ఆ వినాయకుడి ఆశీస్సులతో విజయం చేకూరాలని, వినాయక చవితి రోజున మీరందరూ మనసారా గడపాలని కోరుకుంటూ-మీ శ్రేయభిలాషి..."+inputName+"....వినాయక చవితి శుభాకాంక్షలు.*"
+whatsapplinkEl.href="whatsapp://send?text= *మీరు చేసే ప్రతీ కార్యం ఆ వినాయకుడి ఆశీస్సులతో విజయం చేకూరాలని, వినాయక చవితి రోజున మీరందరూ మనసారా గడపాలని కోరుకుంటూ-మీ శ్రేయభిలాషి..."+inputName.toUpperCase()+"....వినాయక చవితి శుభాకాంక్షలు.*"
