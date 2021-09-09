39c2aca7946b5f3ad3fd272e2f551faf27e6cf0e
nookesh <nookeshkarri7@gmail.com>
2021-09-09 15:17:34 +0530

    Audio added

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let nameEl=document.getElementById("name");
 let inputName=prompt("Enter your Name");
 nameEl.textContent="Hi.. "+inputName.toUpperCase()
 
+let color=["red","blue","yellow","Orange","green"]
+
+let whatsapplinkEl=document.getElementById("whatsapplink")
+whatsapplinkEl.href="whatsapp://send?text= *మీరు చేసే ప్రతీ కార్యం ఆ వినాయకుడి ఆశీస్సులతో విజయం చేకూరాలని, వినాయక చవితి రోజున మీరందరూ మనసారా గడపాలని కోరుకుంటూ-మీ శ్రేయభిలాషి..."+inputName+"....వినాయక చవితి శుభాకాంక్షలు.*"
